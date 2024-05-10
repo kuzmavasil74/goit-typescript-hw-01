79fffc5552ba68ac6142d32c945f0edc1190d2fd
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-05-10 20:06:20 +0200

    basics_tasks_done

---
+++ vite-project/src/basic/3.ts
@@ -0,0 +1,10 @@
+let vslue: string | number
+
+vslue = 'Max'
+vslue = 21
+
+//------------------------------------------------
+let Persi: 'enable' | 'disable'
+
+Persi = 'enable'
+Persi = 'disable'
